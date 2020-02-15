2db3462c2594b76410496fcb4a5e716f1c01e5ef
jendiamond <thejendiamond@gmail.com>
2020-02-14 17:38:16 -0800

    WIP Lesson 9 Communicating Events
    ---
    
    Changes to be committed:
    modified:   index.html
    modified:   main.js
    modified:   product-style.css

--- vm1/main.js
+++ vm1/main.js
@@ -19,57 +19,66 @@ Vue.component( 'product', {
     }
   },
   template: `
-      <div class="product">
-        <div class="product-image">
-          <a :href="url" target="_blank rel=noopener"><img :src="image"></a>
-        </div>
-        <div class="product-info">
-          <h1>{{ title }}</h1>
-          <p>{{ description }}</p>
-          <button v-on:click="addToCart"
-                      :disabled="!inStock"
-                      :class="{ disabledButton: !inStock }">
-                      Add to Cart
-          </button>
-          <br>
-          <button v-on:click="deleteFromCart">Delete From Cart</button>
-          <div class="cart">
-            <p>Cart({{cart}})</p>
+    <div class="product">
+      <div class="product-image">
+        <a :href="url" target="_blank rel=noopener"><img :src="image"></a>
+      </div>
+
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
+        <product-details :details="details"></product-details>
+        <hr>
+
+        <div class="wrapper">
+
+          <div class="column1">
+            <p><strong>Styles:</strong></p>
+            <div v-for="(variant, index) in variants"
+              :key="variant.variantId"
+              class="color-box"
+              :style="{ backgroundColor: variant.variantColor }" >
+              <div @mouseover="updateProduct(index)" class="animal">{{ variant.variantAnimal }}</div>
+            </div>
           </div>
+
+          <div class="column2">
+            <h3>Sizes</h3>
+            <ul>
+              <li v-for="size in sizes">{{ size }}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class='buy-info'>
+          <button v-on:click="addToCart"
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }">
+          Add to Cart
+          </button>
+
+          <br>
+
+          <button v-on:click="deleteFromCart">Delete From Cart</button>
           <p v-if="inventory > 10"
-            :disabled-stock="!inStock"
-            :class="{ disabledStock: !inStock }">
-              In Stock
+          :disabled-stock="!inStock"
+          :class="{ disabledStock: !inStock }">
+          In Stock
           </p>
           <p v-else-if="inventory < 10 && inventory > 0">Almost out of Stock</p>
           <p v-else >Out of Stock</p>
           <p>Shipping: {{ shipping }}</p>
-          <p>User is premium: {{ premium }}</p>
           <p v-if="onSale">{{ onSale }}ON SALE NOW! 🛍</p>
-          <hr>
-          <product-details :details="details"></product-details>
-          <hr>
-          <p><strong>Animals:</strong></p>
-          <div v-for="(variant, index) in variants"
-            :key="variant.variantId"
-            class="color-box"
-            :style="{ backgroundColor: variant.variantColor }" >
-            <p @mouseover="updateProduct(index)">{{ variant.variantAnimal }}</p>
-          </div>
-          <hr>
-          <h3>Sizes</h3>
-          <ul>
-            <li v-for="size in sizes">{{ size }}</li>
-          </ul>
         </div>
       </div>
+    </div>
   `,
   data() {
     return {
       product: 'Animal Paw Crew Socks',
       brand: 'What on Earth',
       description: 'Creepily realistic cat foot socks.',
-      details: ["100% polyester", "Machine washable and dryable", "Printed in the USA", "One size fits up to Men's 13", "Buy 2 or More and Save 20%!"],
+      details: ["100% polyester", "Machine washable and dryable", "Printed in the USA", "Sizes fit up to Men's 13", "Buy 2 or More and Save 20%!"],
       selectedVariant: 0,
       url: 'https://www.whatonearthcatalog.com/CT8119.html',
       inventory: 100,
@@ -103,13 +112,13 @@ Vue.component( 'product', {
           onSale: false
         },
       ],
-      sizes: ["small","medium","large"],
+      sizes: ["Small","Medium","Large"],
       cart: 0,
     }
   },
   methods: {
     addToCart() {
-      this.cart += 1
+      this.$emit('add-to-cart')
     },
     deleteFromCart() {
       this.cart -= 1
@@ -147,5 +156,11 @@ var app = new Vue({
   el: '#app',
   data: {
     premium: true,
+    cart: 0
+  },
+  methods: {
+    updateCart() {
+      this.cart += 1
+    },
   }
 })
